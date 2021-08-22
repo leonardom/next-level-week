378caa131f50b57af078b21518eaf31e28f4c021
GateQuality <ggitenzo@gmail.com>
2021-08-22 07:20:42 +0000

    add test

---
+++ server/src/database/migrations/01_create_items.test.ts
@@ -0,0 +1,7 @@
+import * as _01_create_items from "./01_create_items"
+// @ponicode
+describe("_01_create_items.up", () => {
+    test("0", async () => {
+        await _01_create_items.up(undefined)
+    })
+})
